3f6bb9b5a352784cae3ee016e653eeb5b6b3f55c
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2025-08-17 20:50:43 +0300

    add docker files

--- carseerTask/obj/Debug/net8.0/carseerTask.AssemblyInfo.cs
+++ carseerTask/obj/Debug/net8.0/carseerTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("carseerTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e8be2c1001e6e4946188738225becef6b4d241")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65807efb112649cb78a017529c20d2e60bbeed6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("carseerTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("carseerTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
